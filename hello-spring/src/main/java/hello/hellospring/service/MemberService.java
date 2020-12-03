cf290e14c633fb929d19012b5712c07a7dd0b9f8
Chany <inshakr2@naver.com>
2020-12-04 00:24:01 +0900

    Add Client Service .. Find All Member

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -4,16 +4,17 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-    /*
-    회원가입
+    /**
+    * 회원가입
     */
-    public Long join(Member member){
+    public Long join(Member member) {
         // 중복회원 검증
         validateDuplicateMember(member);
         memberRepository.save(member);
@@ -25,6 +26,13 @@ public class MemberService {
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
     }
 
 
