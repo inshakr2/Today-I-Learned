cda2e22b336e4a9ad49eb2d0b0af0b73ca8ae7a6
Chany <inshakr2@naver.com>
2020-12-05 17:56:04 +0900

    Test Service

---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,26 @@
+package hello.hellospring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceTest;
+
+
+    @Test
+
+    // Test 코드 작성은 한글로 바꿔도 무관하다..
+    // Build시에 실제로 포함되지 않는다.
+    void join() {
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
