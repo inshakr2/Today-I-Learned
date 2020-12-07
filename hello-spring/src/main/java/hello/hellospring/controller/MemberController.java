de6858ee1804201778503c08b2f26f527416092b
Chany <inshakr2@naver.com>
2020-12-08 00:06:11 +0900

    Spring bin - @Configuration & @Bean

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -13,8 +13,5 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
-    // 이 상태로 실행하면 오류.
-    // 스프링컨테이너에서 해당 MemberService를 가져오는데,
-    // Controller는 스프링이 뜰 때 등록이 되지만,
-    // MemberService는 스프링이 알 수 있는 방법이 없다.
+
 }
