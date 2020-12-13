01ba8205163da90b2ce56929f9086843eb5e8668
Chany <inshakr2@naver.com>
2020-12-13 14:01:58 +0900

    H2 Native JDBC Bean

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     // 자바 코드로 직접 스프링 빈에 등록하기..
     // configuration 애노테이션을 만들고, Bean 애노테이션을 달아준다.
@@ -20,7 +31,11 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
+
+
 
 }
