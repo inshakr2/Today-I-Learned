8e0285dd61f5b1273b746f33b71cb0718f862e5a
Chany <inshakr2@naver.com>
2020-12-15 22:22:28 +0900

    jpa setting ... Entity / repository

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,5 +1,8 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
     public Long getId() {
@@ -17,8 +20,11 @@ public class Member {
     public void setName(String name) {
         this.name = name;
     }
-
+                            // DB에서 생성되는 PK id 값 사용
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+//    @Column(name="username") 만약 DB 컬럼명이 username인경우 @Column을 통해 맵핑
     private String name;
 
 }
---
+++ hello-spring/src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -0,0 +1,46 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository {
+
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+    // EntityManager --- spring boot가 em을 만들어주는데,, 요거를 injection받으면 된다.
+
+
+    @Override
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = em.createQuery("select m from Member m where m.name= :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+        return result.stream().findAny();
+
+    }
+
+    @Override
+    public List<Member> findAll() {     // 등록한 Entity에서 조회
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+}
