422f5b8dfc3b5d7348c6d3ceec9157cef0f12d9d
Chany <inshakr2@naver.com>
2021-01-30 17:49:11 +0900

    AutoConfiguration Training - add properties

--- spring-boot-starter/src/main/java/chany/HolomanConfiguration.java
+++ spring-boot-starter/src/main/java/chany/HolomanConfiguration.java
@@ -1,16 +1,21 @@
 package chany;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(HolomanProperties.class)
 public class HolomanConfiguration {
 
     @Bean
-    public Holoman holoman() {
+    @ConditionalOnMissingBean
+    public Holoman holoman(HolomanProperties properties) {
         Holoman holoman = new Holoman();
-        holoman.setHowlong(5);
-        holoman.setName("chany");
+        holoman.setHowlong(properties.getHowLong());
+        holoman.setName(properties.getName());
         return holoman;
     }
 }
