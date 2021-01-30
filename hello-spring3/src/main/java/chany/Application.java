b0a7edb2296133b07e46e841e38c65bcc60007f4
Chany <inshakr2@naver.com>
2021-01-30 17:04:04 +0900

    AutoConfiguration Training

--- hello-spring3/src/main/java/chany/Application.java
+++ hello-spring3/src/main/java/chany/Application.java
@@ -1,11 +1,16 @@
 package chany;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.run(args);
     }
 }
