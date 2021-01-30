fc09de1dc20f43353d1d0296e90f2b912b8f50e5
Chany <inshakr2@naver.com>
2021-01-30 18:40:24 +0900

    Change Server to jetty

--- hello-spring3/src/main/java/chany/Application.java
+++ hello-spring3/src/main/java/chany/Application.java
@@ -1,49 +1,15 @@
 package chany;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 
+@SpringBootApplication
 public class Application {
-    public static void main(String[] args) throws LifecycleException {
-        Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8888);
+    public static void main(String[] args) {
 
-        Context context = tomcat.addContext("/", "/");
-
-        HttpServlet servlet = new HttpServlet() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                PrintWriter writer = resp.getWriter();
-                writer.println("<html><head><title>");
-                writer.println("Hey, tomcat");
-                writer.println("</title></head>");
-                writer.println("<body><h1>Hello</h1></body>");
-                writer.println("</html>");
-
-            }
-        };
-        String servletName = "hello";
-        tomcat.addServlet("/", servletName, servlet);
-        context.addServletMappingDecoded("/hello", servletName);
-
-        tomcat.start();
-        tomcat.getServer().await();
+        SpringApplication.run(Application.class, args);
 
     }
 }
