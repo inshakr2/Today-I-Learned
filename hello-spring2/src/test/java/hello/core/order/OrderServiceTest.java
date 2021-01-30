7d39155571ee7c474b01271f1745cd482bd9b208
Chany <inshakr2@naver.com>
2021-01-30 15:06:46 +0900

    Test Commit

--- hello-spring2/src/test/java/hello/core/order/OrderServiceTest.java
+++ hello-spring2/src/test/java/hello/core/order/OrderServiceTest.java
@@ -15,6 +15,7 @@ public class OrderServiceTest {
     @Test
     void createOrder(){
         Long memberId = 1L;
+
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
