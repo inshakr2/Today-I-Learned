6cc0bdd1daf0dd5d5c91bc5b81e3ad7fa7724d6a
Chany <inshakr2@naver.com>
2021-01-31 22:13:43 +0900

    Lecture - Test with OutputCaptureRule

--- spring-test-demo/src/test/java/chany/sample/SampleControllerWebMvcTest.java
+++ spring-test-demo/src/test/java/chany/sample/SampleControllerWebMvcTest.java
@@ -1,13 +1,16 @@
 package chany.sample;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.system.OutputCaptureRule;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -15,6 +18,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebMvcTest(SampleController.class)
 public class SampleControllerWebMvcTest {
+
+    @Rule
+    public OutputCaptureRule outputCaptureRule = new OutputCaptureRule();
+
 
     @MockBean
     SampleService mockSampleService;
@@ -28,5 +35,9 @@ public class SampleControllerWebMvcTest {
 
         mockMvc.perform(get("/hello"))
                 .andExpect(content().string("hello CHANY"));
+
+        assertThat(outputCaptureRule.toString())
+                .contains("yeol")
+                .contains("yeol2");
     }
 }
