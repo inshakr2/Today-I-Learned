1076ae7cf03dc7a0b357ad8d031c33ed904ac2ad
Chany <inshakr2@naver.com>
2021-01-31 16:15:27 +0900

    Lecture - external setting Properties, regist Bean

--- Spring-init/src/main/java/chany/SampleRunner.java
+++ Spring-init/src/main/java/chany/SampleRunner.java
@@ -1,5 +1,6 @@
 package chany;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -8,18 +9,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleRunner implements ApplicationRunner {
 
-    @Value("${chany.fullname}")
-    private String name;
-
-    @Value("${chany.age}")
-    private int age;
+    @Autowired
+    ChanyProperties chanyProperties;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
         System.out.println("===================");
-        System.out.println(name);
-        System.out.println(age);
+        System.out.println("Full Name = " + chanyProperties.getFullName());
+        System.out.println("Name = " + chanyProperties.getName());
+        System.out.println("age = " + chanyProperties.getAge());
         System.out.println("===================");
     }
 }
--- Spring-init/src/main/java/chany/SpringInitApplication.java
+++ Spring-init/src/main/java/chany/SpringInitApplication.java
@@ -3,6 +3,7 @@ package chany;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class SpringInitApplication {
