abbc657eb36c01b98a385acee039bac688ddc1e9
Chany <inshakr2@naver.com>
2021-02-06 09:13:55 +0900

    Lecture - testing model view with containsString

--- spring-thymeleaf/src/test/java/chany/SampleControllerTest.java
+++ spring-thymeleaf/src/test/java/chany/SampleControllerTest.java
@@ -8,8 +8,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
@@ -23,6 +25,8 @@ public class SampleControllerTest {
     public void hello() throws Exception {
         mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().string(containsString("chany")))
                 .andExpect(view().name("hello"))
                 .andExpect(model().attribute("name", "chany"));
     }
