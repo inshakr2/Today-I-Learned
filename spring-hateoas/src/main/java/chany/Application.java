c8177409f6e4a35caeba0e47ca0e3bc6105d4ab0
Chany <inshakr2@naver.com>
2021-02-10 00:15:13 +0900

    Lecture - New prj for Hateoas

---
+++ spring-hateoas/src/main/java/chany/Application.java
@@ -0,0 +1,10 @@
+package chany;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
