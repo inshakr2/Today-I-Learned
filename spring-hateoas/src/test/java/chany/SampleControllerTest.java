fcb7bd684df4296c6c9c9056fceb6f7910bdbc40
Chany <inshakr2@naver.com>
2021-02-11 11:41:07 +0900

    Lecture - New prj for CORS lecture

--- spring-hateoas/src/test/java/chany/SampleControllerTest.java
+++ spring-hateoas/src/test/java/chany/SampleControllerTest.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(SampleController.class)
-class SampleControllerTest {
+public class SampleControllerTest {
 
     @Autowired
     MockMvc mockMvc;
