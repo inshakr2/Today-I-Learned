347a54f44c75801265f85224211769111dbec602
Chany <inshakr2@naver.com>
2021-02-16 00:48:22 +0900

    Lecture - Test Mongo with Embed mongo

--- spring-mongodb/src/main/java/chany/account/AccountRepository.java
+++ spring-mongodb/src/main/java/chany/account/AccountRepository.java
@@ -3,5 +3,10 @@ package chany.account;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends MongoRepository<Account, String> {
+
+    Optional<Account> findByEmail(String email);
+
 }
