bc07a66d56f825187ebe342602b53785263d31de
Chany <inshakr2@naver.com>
2021-02-16 00:35:36 +0900

    Lecture - Test Mongo with docker

--- spring-mongodb/src/main/java/chany/Application.java
+++ spring-mongodb/src/main/java/chany/Application.java
@@ -1,11 +1,36 @@
 package chany;
 
+import chany.account.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class Application {
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> {
+            Account account = new Account();
+            account.setEmail("chany@.com");
+            account.setUsername("chany");
+
+            mongoTemplate.insert(account);
+
+            System.out.println("Fin");
+        };
+    }
+
 }
