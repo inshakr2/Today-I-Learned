58c110ad1b60e17d4e45cc5d486d43f0c0b28238
Chany <inshakr2@naver.com>
2021-02-15 23:49:08 +0900

    Lecture - Test Redis with Repository

--- spring-redis/src/main/java/chany/RedisRunner.java
+++ spring-redis/src/main/java/chany/RedisRunner.java
@@ -1,5 +1,7 @@
 package chany;
 
+import chany.account.Account;
+import chany.account.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -7,11 +9,16 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class RedisRunner implements ApplicationRunner {
 
     @Autowired
     StringRedisTemplate redisTemplate;
+
+    @Autowired
+    AccountRepository accountRepository;
 
 
     @Override
@@ -20,5 +27,15 @@ public class RedisRunner implements ApplicationRunner {
         values.set("cy","chany");
         values.set("spring", "2.0");
         values.set("hello", "redis");
+
+        Account account = new Account();
+
+        account.setUsername("chany");
+        account.setEmail("chany@@");
+
+        accountRepository.save(account);
+        Optional<Account> findAccount = accountRepository.findById(account.getId());
+        System.out.println(findAccount.get().getUsername());
+        System.out.println(findAccount.get().getEmail());
     }
 }
