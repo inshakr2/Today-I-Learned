b696d3f76ca38d79d746446836518b65c58a6d1d
Chany <inshakr2@naver.com>
2021-02-19 19:06:14 +0900

    Lecture - Setting for spring boot security

---
+++ spring-security/src/main/java/chany/HomeController.java
@@ -0,0 +1,13 @@
+package chany;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
