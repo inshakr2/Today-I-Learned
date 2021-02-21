57813e2484800d88b3517f84e333450aa80ce19d
Chany <inshakr2@naver.com>
2021-02-21 13:50:48 +0900

    Lecture - basic security concept

--- spring-security/src/test/java/chany/HomeControllerTest.java
+++ spring-security/src/test/java/chany/HomeControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,9 +23,18 @@ public class HomeControllerTest {
 
     @Test
     public void hello() throws Exception {
-        mockMvc.perform(get("/hello"))
+        mockMvc.perform(get("/hello")
+                    .accept(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
                 .andExpect(view().name("hello"));
     }
+
+    @Test
+    public void my() throws Exception {
+        mockMvc.perform(get("/my"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("my"));
+    }
+
 
 }
