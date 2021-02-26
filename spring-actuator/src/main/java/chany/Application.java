b0693e25d3930a29ddfbdc51464b1d4f1317bf3f
Chany <inshakr2@naver.com>
2021-02-26 11:14:33 +0900

    Lecture - New prj for About Actuator

---
+++ spring-actuator/src/main/java/chany/Application.java
@@ -0,0 +1,17 @@
+package chany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Application {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
