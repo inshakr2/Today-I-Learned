ae127260c31c54e22fd0cdbfbf58779a31ba13c1
Chany <inshakr2@naver.com>
2021-03-07 16:58:38 +0900

    REST API Lecture - Integrate Spring REST Docs

---
+++ rest-api-with-spring/src/test/java/chany/common/RestDocsConfiguration.java
@@ -0,0 +1,18 @@
+package chany.common;
+
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+@TestConfiguration // 테스트에서만 사용하는 Configuration
+public class RestDocsConfiguration {
+
+    @Bean
+    public RestDocsMockMvcConfigurationCustomizer restDocsMockMvcBuilderCustomizer() {
+        return configurer -> configurer.operationPreprocessors()
+                .withRequestDefaults(prettyPrint())
+                .withResponseDefaults(prettyPrint());
+    }
+}
