dd9e33c5fd855f9c853c1377feef0c3e127e3d2f
Chany <inshakr2@naver.com>
2021-03-24 00:01:41 +0900

    REST API Lecture - Test SpringSecurity with new Account

--- rest-api-with-spring/src/main/java/chany/configs/AppConfig.java
+++ rest-api-with-spring/src/main/java/chany/configs/AppConfig.java
@@ -1,10 +1,18 @@
 package chany.configs;
 
+import chany.accounts.Account;
+import chany.accounts.AccountRole;
+import chany.accounts.AccountService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @Configuration
 public class AppConfig {
@@ -19,4 +27,25 @@ public class AppConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+
+            @Autowired
+            AccountService accountService;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+
+                // test용 계정 임시 생성
+                Account chany = Account.builder()
+                        .email("chany@kakao.com")
+                        .password("chany")
+                        .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                        .build();
+                accountService.saveAccount(chany);
+
+            }
+        };
+    }
 }
