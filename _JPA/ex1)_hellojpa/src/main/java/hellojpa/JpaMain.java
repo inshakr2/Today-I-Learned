0c30cc89be02cbcf7d8c95647b4087f6ae531bfc
Chany <inshakr2@naver.com>
2021-04-06 21:18:48 +0900

    JPA Lecture - Hello JPA

--- _JPA/ex1)_hellojpa/src/main/java/hellojpa/JpaMain.java
+++ _JPA/ex1)_hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,39 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
 
-        EntityManagerFactory em = Persistence.createEntityManagerFactory("hello");
+        // Factory 생성
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // Manager 생성
+        EntityManager em = emf.createEntityManager();
 
-        EntityManager entityManager = em.createEntityManager();
-        entityManager.close();
-        em.close();
+        // Transaction
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+//            Member findMember = em.find(Member.class, 1L);
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member = " + member.getName());
+            }
+
+            tx.commit();
+        }catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
     }
 }
