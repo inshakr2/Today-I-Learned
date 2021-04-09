636bf37db92afc5d35ff3379cfbd3112082675b9
Chany <inshakr2@naver.com>
2021-04-09 14:29:21 +0900

    JPA Lecture - Entity Mapping

--- _JPA/ex1)_hellojpa/src/main/java/hellojpa/Member.java
+++ _JPA/ex1)_hellojpa/src/main/java/hellojpa/Member.java
@@ -1,13 +1,15 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
+@Entity(name = "Member")
 public class Member {
 
     @Id
     private Long id;
+    @Column(name = "name", nullable = false, columnDefinition = "varchar(100) default 'EMPTY")
     private String name;
 
     public Long getId() {
