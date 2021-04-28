ea74cdf64132b0c30c3758dad6e47c83317e7eab
Chany <inshakr2@naver.com>
2021-04-28 20:40:09 +0900

    JPA Lecture - Equivalence Comparison

--- _JPA/ex1)_hellojpa/src/main/java/hellojpa/embeded/Address.java
+++ _JPA/ex1)_hellojpa/src/main/java/hellojpa/embeded/Address.java
@@ -1,6 +1,7 @@
 package hellojpa.embeded;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
@@ -28,5 +29,20 @@ public class Address {
 
     public String getZipcode() {
         return zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity()) &&
+                Objects.equals(getStreet(), address.getStreet()) &&
+                Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
     }
 }
