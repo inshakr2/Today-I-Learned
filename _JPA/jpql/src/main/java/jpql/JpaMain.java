b53e55dd4b32b6409be6a4f5073fd6af1e2f4594
Chany <inshakr2@naver.com>
2021-05-23 13:07:09 +0900

    JPA Lecture - New prj for JPQL

---
+++ _JPA/jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,34 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        // Factory 생성
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // Manager 생성
+        EntityManager em = emf.createEntityManager();
+
+        // Transaction
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            tx.commit();
+        }catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+
+}
+
