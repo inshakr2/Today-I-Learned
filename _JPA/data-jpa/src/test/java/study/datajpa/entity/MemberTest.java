6986e45d4a7b9529d8157b05c700225bd14eae3f
Chany <inshakr2@naver.com>
2021-07-20 00:30:45 +0900

    Lecture data-jpa : Auditing

--- _JPA/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ _JPA/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -20,6 +21,9 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity() {
@@ -47,6 +51,21 @@ class MemberTest {
             System.out.println("member = " + member);
             System.out.println(" -> member.team = " + member.getTeam());
         }
+
+    }
+
+    @Test
+    public void JpaEventBaseEntity() throws InterruptedException {
+        Member member = new Member("member1");
+        memberRepository.save(member); // @PrePersist
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush(); //@PreUpdate
+        em.clear();
+
+        Member findMember = memberRepository.findById(member.getId()).get();
 
     }
 
