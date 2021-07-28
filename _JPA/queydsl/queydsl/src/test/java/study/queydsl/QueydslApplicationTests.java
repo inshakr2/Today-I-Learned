fca8f6bb98951e2d140b8371be71da6dffb33156
Chany <inshakr2@naver.com>
2021-07-28 23:41:26 +0900

    Lecture Query dsl : Setting Querydsl

--- _JPA/queydsl/queydsl/src/test/java/study/queydsl/QueydslApplicationTests.java
+++ _JPA/queydsl/queydsl/src/test/java/study/queydsl/QueydslApplicationTests.java
@@ -1,13 +1,40 @@
 package study.queydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.queydsl.entity.Hello;
+import study.queydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class QueydslApplicationTests {
+
+	@PersistenceContext
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+
 	}
 
 }
