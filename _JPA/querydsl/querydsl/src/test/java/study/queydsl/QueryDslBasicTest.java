ff9fd8a0b2db9ac91045c0e4d1ad92f0975d5f75
Chany <inshakr2@naver.com>
2021-08-11 00:57:57 +0900

    Lecture Query dsl : Bulk Update

--- _JPA/querydsl/querydsl/src/test/java/study/queydsl/QueryDslBasicTest.java
+++ _JPA/querydsl/querydsl/src/test/java/study/queydsl/QueryDslBasicTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.queydsl.dto.MemberDto;
 import study.queydsl.dto.QMemberDto;
@@ -640,5 +641,36 @@ public class QueryDslBasicTest {
 
     private BooleanExpression allEq(String usernameCond, Integer ageCond) {
         return usernameEq(usernameCond).and(ageEq(ageCond));
+    }
+
+    @Test
+    @Commit
+    public void bulkUpdate() {
+
+        long count = queryFactory
+                .update(member)
+                .set(member.username, "비회원")
+                .where(member.age.lt(28))
+                .execute();
+
+        em.flush();
+        em.clear();
+    }
+
+    @Test
+    public void bulkAdd() {
+
+        long count = queryFactory
+                .update(member)
+                .set(member.age, member.age.add(1))
+                .execute();
+    }
+
+    @Test
+    public void bulkDelete() {
+        queryFactory
+                .delete(member)
+                .where(member.age.gt(18))
+                .execute();
     }
 }
