87788557c229b18412bc968d8a73759c67d9cffd
Chany <inshakr2@naver.com>
2021-08-17 23:52:15 +0900

    Lecture Query dsl : Rest Controller

---
+++ _JPA/querydsl/querydsl/src/main/java/study/queydsl/controller/MemberController.java
@@ -0,0 +1,22 @@
+package study.queydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.queydsl.dto.MemberSearchCondition;
+import study.queydsl.dto.MemberTeamDto;
+import study.queydsl.repository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+}
