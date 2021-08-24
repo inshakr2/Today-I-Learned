63afc24bb321a074df8e102076245db674b9c1c9
Chany <inshakr2@naver.com>
2021-08-24 22:13:50 +0900

    Practice (Board) - New prj for Practice & setting

---
+++ board/src/main/java/chany/board/controller/BoardController.java
@@ -0,0 +1,19 @@
+package chany.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/")
+    public String list() {
+        return "board/list.html";
+    }
+
+    @GetMapping("/post")
+    public String post() {
+        return "board/post.html";
+    }
+}
