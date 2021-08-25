24fa7affa4c60e22b6aa8f5b1fa18fd3c58866dd
Chany <inshakr2@naver.com>
2021-08-25 21:37:20 +0900

    Practice (Board) - create basic class & repository

---
+++ board/src/main/java/chany/board/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package chany.board.repository;
+
+import chany.board.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
