7c009e2ea024748ef3cecaed5cfe9aa2e6411a8b
Chany <inshakr2@naver.com>
2021-12-24 19:22:03 +0900

    Spring Core Lecture - 9. Bean Scope - Prototype

---
+++ core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,35 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+
+        Assertions.assertThat(bean1).isNotSameAs(bean2);
+
+        ac.close();
+    }
+
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
