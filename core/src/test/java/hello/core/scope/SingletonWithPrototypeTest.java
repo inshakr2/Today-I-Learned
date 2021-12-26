8edce8628c0fd386c074d4f5735c197d4af5d3bf
Chany <inshakr2@naver.com>
2021-12-26 16:47:02 +0900

    Spring Core Lecture - 9. Bean Scope - Prototype problem

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -1,6 +1,8 @@
 package hello.core.scope;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -19,6 +21,38 @@ public class SingletonWithPrototypeTest {
         bean2.addCount();
 
     }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(2);
+
+
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+
+        private final PrototypeBean prototypeBean;
+
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
 
     @Scope("prototype")
     static class PrototypeBean {
