4b916c00a01fd46e55bb1d21bd18412772a37493
Chany <inshakr2@naver.com>
2021-12-28 22:39:32 +0900

    Spring Core Lecture - 9. Bean Scope - ObjectProvider

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -32,7 +33,7 @@ public class SingletonWithPrototypeTest {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
 
 
     }
@@ -40,14 +41,12 @@ public class SingletonWithPrototypeTest {
     @Scope("singleton")
     static class ClientBean {
 
-        private final PrototypeBean prototypeBean;
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
