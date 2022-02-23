24d5fbaf86440d1d77870d1f0025ca8920934dac
Chany <inshakr2@naver.com>
2022-02-23 13:55:46 +0900

    Spring MVC Lecture - 5. Spring MVC Practice - Fix

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -80,8 +80,19 @@ public class BasicItemController {
         return "redirect:/basic/items/" + item.getId();
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+
+        Item savedItem = itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
 
         Item savedItem = itemRepository.save(item);
 
