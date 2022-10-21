0a4375fed5473c2274f14ef078ebeaf2c20d9996
Chany <inshakr2@naver.com>
2022-10-21 21:43:29 +0900

    MVC Lecture - Bean Validation

--- validation/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ validation/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -3,20 +3,16 @@ package hello.itemservice;
 import hello.itemservice.web.validation.ItemValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-//public class ItemServiceApplication implements WebMvcConfigurer {
 public class ItemServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
-//	@Override
-//	public Validator getValidator() {
-//		return new ItemValidator();
-//	}
 
 }
