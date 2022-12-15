6e0f646271f6e35e594eba2092d6accd415aac1a
Chany <inshakr2@naver.com>
2022-12-16 00:02:43 +0900

    MVC Lecture - ControllerAdvice 적용

--- exception/src/main/java/hello/exception/exhandler/ApiExceptionV2Controller.java
+++ exception/src/main/java/hello/exception/exhandler/ApiExceptionV2Controller.java
@@ -15,13 +15,11 @@ import org.springframework.web.servlet.ModelAndView;
 public class ApiExceptionV2Controller {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class)
     public ErrorResult illegalExHandle(IllegalArgumentException e) {
         log.error("[exceptionHandle] ex", e);
         return new ErrorResult("BAD", e.getMessage());
     }
 
-    @ExceptionHandler
     public ResponseEntity<ErrorResult> userExHandle(UserException e) {
         log.error("[exceptionHandle] ex", e);
         ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
@@ -29,13 +27,11 @@ public class ApiExceptionV2Controller {
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
     public ErrorResult exHandle(Exception e) {
         log.error("[exceptionHandle] ex", e);
         return new ErrorResult("EX", "내부 오류");
     }
 
-    @ExceptionHandler(ViewException.class)
     public ModelAndView ex(ViewException e) {
         log.info("exception e", e);
         return new ModelAndView("error");
