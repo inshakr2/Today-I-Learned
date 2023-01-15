d4b2eaa877b0c1717d1acaaa8abc7365404e1866
Chany <inshakr2@naver.com>
2023-01-15 16:07:08 +0900

    Lecture (Concurrency Issue) - Pessimistic Lock

--- stock/src/main/java/com/example/stock/repository/StockRepository.java
+++ stock/src/main/java/com/example/stock/repository/StockRepository.java
@@ -2,8 +2,18 @@ package com.example.stock.repository;
 
 import com.example.stock.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.LockModeType;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM Stock s Where s.id = :id")
+    Stock findByIdWithPessimisticLock(@Param("id") Long id);
+
 }
