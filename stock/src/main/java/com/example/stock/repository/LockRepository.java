446cab2d320ef742ada421a37d27cecd0196cd92
Chany <inshakr2@naver.com>
2023-01-17 00:21:47 +0900

    Lecture (Concurrency Issue) - Named Lock

---
+++ stock/src/main/java/com/example/stock/repository/LockRepository.java
@@ -0,0 +1,19 @@
+package com.example.stock.repository;
+
+import com.example.stock.domain.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LockRepository extends JpaRepository<Stock, Long> {
+
+    @Query(value = "SELECT get_lock(:key, 3000)", nativeQuery = true)
+    void getLock(@Param("key") String key);
+
+    @Query(value = "SELECT release_lock(:key)", nativeQuery = true)
+    void releaseLock(@Param("key") String key);
+
+
+}
