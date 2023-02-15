f8599a6327e5c7a6ad160e159ef50342c35a2152
Chany <inshakr2@naver.com>
2023-02-16 00:19:27 +0900

    Lecture (API-Exception) - HandlerExceptionResolver 활용

--- api-exception/src/main/java/hello/apiexception/WebConfig.java
+++ api-exception/src/main/java/hello/apiexception/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.apiexception;
 
 import hello.apiexception.resolver.MyHandlerExceptionResolver;
+import hello.apiexception.resolver.UserHandlerExceptionResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,5 +15,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
         resolvers.add(new MyHandlerExceptionResolver());
+        resolvers.add(new UserHandlerExceptionResolver());
     }
 }
