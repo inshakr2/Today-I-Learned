31068963a71c095fa5ccf530b5f5539914ba1c62
Chany <inshakr2@naver.com>
2023-03-05 16:53:07 +0900

    Spring Upload Lecture - spring MultiPart Upload

--- spring-upload/src/main/java/hello/springupload/controller/ServletUploadControllerV2.java
+++ spring-upload/src/main/java/hello/springupload/controller/ServletUploadControllerV2.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,6 +54,13 @@ public class ServletUploadControllerV2 {
             InputStream inputStream = part.getInputStream();
             String body = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
             log.info("body = {}", body);
+
+            if (StringUtils.hasText(part.getSubmittedFileName())) {
+                String fullPath = fileDir + part.getSubmittedFileName();
+                log.info("File Save fullPath = {}", fullPath);
+                part.write(fullPath);
+
+            }
 
         }
 
