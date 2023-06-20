64565890d51d57bc52076e738987b602a197d614
Chany <inshakr2@naver.com>
2023-06-20 22:42:08 +0900

    Dockerfile 실습 : node app

---
+++ Server/CI/docker/hello-node/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const PORT = 3201;
+
+// APP
+const app = express();
+app.get('/', (req, res) => {
+    res.send('hello Node App');
+});
+
+app.listen(PORT);
+console.log(`Server Running on http://localhost:${PORT}`);
