8aff45efabccf983512565fa688075babd494471
Chany <inshakr2@gmail.com>
2024-06-09 23:55:43 +0900

    CustomAuthenticationEntryPoint

--- Spring/spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ Spring/spring-security-oauth2/src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,18 +9,37 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(
                 req -> req.anyRequest().authenticated()
             )
-            .formLogin(withDefaults())
+            .httpBasic(withDefaults())
+            .exceptionHandling(httpSecurityExceptionHandlingConfigurer -> {
+                httpSecurityExceptionHandlingConfigurer.authenticationEntryPoint(customAuthenticationEntryPoint);
+            });
 //            .apply(new CustomSecurityConfigurer().setFlag(false));
-            .with(new CustomSecurityConfigurer(), customSecurityConfigurer -> customSecurityConfigurer.setFlag(false));
+//            .with(new CustomSecurityConfigurer(), customSecurityConfigurer -> customSecurityConfigurer.setFlag(false));
 
         return http.build();
     }
+
+//    @Bean
+//    SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(
+//                        req -> req.anyRequest().authenticated()
+//                )
+//                .httpBasic(withDefaults());
+//            .apply(new CustomSecurityConfigurer().setFlag(false));
+//            .with(new CustomSecurityConfigurer(), customSecurityConfigurer -> customSecurityConfigurer.setFlag(false));
+//
+//        return http.build();
+//    }
 }
