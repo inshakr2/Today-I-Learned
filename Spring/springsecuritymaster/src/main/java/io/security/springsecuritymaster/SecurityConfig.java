d63bb19c50ebba2d1be6ae24300359f5c5bd0962
Chany <inshakr2@gmail.com>
2024-09-11 22:34:50 +0900

    AuthenticationManager

--- Spring/springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ Spring/springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -2,7 +2,9 @@ package io.security.springsecuritymaster;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -10,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -17,12 +20,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
 //                .httpBasic(Customizer.withDefaults());
-                .httpBasic(basic -> basic.authenticationEntryPoint(new CustmAuthenticationEntryPoint()));
+//                .httpBasic(basic -> basic.authenticationEntryPoint(new CustmAuthenticationEntryPoint()));
+//                .formLogin(Customizer.withDefaults())
+                .authenticationManager(authenticationManager)
+                .addFilterBefore(customAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    public CustomAuthenticationFilter customAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
+        customAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        return customAuthenticationFilter;
     }
 
     @Bean
