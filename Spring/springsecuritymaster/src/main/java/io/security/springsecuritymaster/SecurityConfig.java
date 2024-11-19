785b37de516b8a8277e48ea84070e4ee9b8ffa94
Chany <inshakr2@gmail.com>
2024-11-19 16:52:16 +0900

    SessionManagementFilter / ConcurrentSessionFilter

--- Spring/springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ Spring/springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -7,12 +7,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -28,19 +24,12 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated())
-//                .httpBasic(Customizer.withDefaults());
-//                .httpBasic(basic -> basic.authenticationEntryPoint(new CustmAuthenticationEntryPoint()));
                 .formLogin(Customizer.withDefaults());
-
-//                .authenticationManager(authenticationManager)
-//                .addFilterBefore(customAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class);
 
         http.sessionManagement(
                 session -> session
-                        .invalidSessionUrl("/invalidSessionUrl")
                         .maximumSessions(1)
                         .maxSessionsPreventsLogin(true)
-                        .expiredUrl("/expired")
         );
 
 
