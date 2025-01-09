e756f2ec31b92cfdf99194f5b331f1a021de90d0
Chany <inshakr2@gmail.com>
2025-01-09 10:54:11 +0900

    UserService Data init

---
+++ MSA/user-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users (email,encrypted_pwd,name,user_id,id)
+    values ('email@naver.com','$2a$10$j0ACsdIVtqOGyF/zWl7BeeazyboKDyvKoDAYNN7FnZUGUVRMlmZta','chany','f6be9163-024a-44c8-9132-1b34c6b319fe',default);
+-- 1234
