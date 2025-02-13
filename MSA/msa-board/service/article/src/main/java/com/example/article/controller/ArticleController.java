71b1aea7a47ff05f69426e7d0d64836208106a01
Chany <inshakr2@gmail.com>
2025-02-14 00:13:18 +0900

    [MSA Traffic Board]  Article 기본 기능 생성

---
+++ MSA/msa-board/service/article/src/main/java/com/example/article/controller/ArticleController.java
@@ -0,0 +1,37 @@
+package com.example.article.controller;
+
+import com.example.article.service.ArticleService;
+import com.example.article.service.request.ArticleCreateRequest;
+import com.example.article.service.request.ArticleUpdateRequest;
+import com.example.article.service.response.ArticleResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ArticleController {
+    private final ArticleService articleService;
+
+    @GetMapping("/v1/articles/{articleId}")
+    public ArticleResponse read(@PathVariable Long articleId) {
+        return articleService.read(articleId);
+    }
+
+    @PostMapping("/v1/articles")
+    public ArticleResponse create(@RequestBody ArticleCreateRequest request) {
+        return articleService.create(request);
+    }
+
+    @PutMapping("/v1/articles/{articleId}")
+    public ArticleResponse update(@PathVariable Long articleId, @RequestBody ArticleUpdateRequest request) {
+        return articleService.update(articleId, request);
+    }
+
+    @DeleteMapping("/v1/articles/{articleId}")
+    public void delete(@PathVariable Long articleId) {
+        articleService.delete(articleId);
+    }
+
+}
