c5ce1478a32a3a5e7f150575cf6a96c7d216f025
Chany <inshakr2@gmail.com>
2025-03-14 15:49:27 +0900

    [MSA Traffic Board] 게시글 개수 기능

--- MSA/msa-board/service/article/src/main/java/com/example/article/service/ArticleService.java
+++ MSA/msa-board/service/article/src/main/java/com/example/article/service/ArticleService.java
@@ -1,7 +1,9 @@
 package com.example.article.service;
 
 import com.example.article.entity.Article;
+import com.example.article.entity.BoardArticleCount;
 import com.example.article.repository.ArticleRepository;
+import com.example.article.repository.BoardArticleCountRepository;
 import com.example.article.service.request.ArticleCreateRequest;
 import com.example.article.service.request.ArticleUpdateRequest;
 import com.example.article.service.response.ArticlePageResponse;
@@ -19,12 +21,20 @@ public class ArticleService {
 
     private final Snowflake snowflake = new Snowflake();
     private final ArticleRepository articleRepository;
+    private final BoardArticleCountRepository boardArticleCountRepository;
 
     @Transactional
     public ArticleResponse create(ArticleCreateRequest request) {
         Article article = articleRepository.save(
                 Article.create(snowflake.nextId(), request.getTitle(), request.getContent(), request.getBoardId(), request.getWriterId())
         );
+
+        int result = boardArticleCountRepository.increase(request.getBoardId());
+        if (result == 0) {
+            boardArticleCountRepository.save(
+                    BoardArticleCount.init(request.getBoardId(), 1L)
+            );
+        }
 
         return ArticleResponse.from(article);
     }
@@ -43,7 +53,10 @@ public class ArticleService {
 
     @Transactional
     public void delete(Long articleId) {
-        articleRepository.deleteById(articleId);
+        Article article = articleRepository.findById(articleId).orElseThrow();
+        articleRepository.delete(article);
+
+        boardArticleCountRepository.decrease(article.getBoardId());
     }
 
     public ArticlePageResponse readAll(Long boardId, Long page, Long pageSize) {
@@ -61,5 +74,11 @@ public class ArticleService {
                 articleRepository.findAllInfiniteScroll(boardId, pageSize) :
                 articleRepository.findAllInfiniteScroll(boardId, pageSize, lastArticleId);
         return articles.stream().map(ArticleResponse::from).toList();
+    }
+
+    public Long count(Long boardId) {
+        return boardArticleCountRepository.findById(boardId)
+                .map(BoardArticleCount::getArticleCount)
+                .orElse(0L);
     }
 }
--- MSA/msa-board/service/article/src/main/resources/db/mysql-schema.sql
+++ MSA/msa-board/service/article/src/main/resources/db/mysql-schema.sql
@@ -7,3 +7,10 @@ create table article (
     created_at datetime not null,
     modified_at datetime not null
 );
+
+create index idx_board_id_article_id on article(board_id asc, article_id desc);
+
+create table board_article_count (
+    board_id bigint not null primary key,
+    article_count bigint not null
+);
